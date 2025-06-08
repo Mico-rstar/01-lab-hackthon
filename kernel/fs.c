1c7803b796a11d81333caa9193d538366ddd891a
Mico-rstar <2114678406@qq.com>
2025-06-08 13:08:05 +0800

    add bigfile

--- kernel/fs.c
+++ kernel/fs.c
@@ -368,6 +368,53 @@ iunlockput(struct inode *ip)
   iunlock(ip);
   iput(ip);
 }
+int power(int base, int exponent)
+{
+  int result = 1;
+  for (int i = 0; i < exponent; i++)
+  {
+    result *= base;
+  }
+  return result;
+}
+
+// Return the disk block address of the nth block in inode ip.
+static uint
+bwalk(struct inode *ip, uint *cur, uint off, ushort level, uint clevel)
+{
+  uint addr;
+  // Load indirect block
+  if(*cur==0)
+    *cur = balloc(ip->dev);
+  if(*cur==0) return 0;
+  struct buf *bp = bread(ip->dev, *cur);
+  uint *a = (uint *)bp->data;
+  uint toff = off / power(NINDIRECT, level - clevel);
+  if ((addr = a[toff]) == 0)
+  {
+
+    addr = balloc(ip->dev);
+
+    if (addr)
+    {
+      a[toff] = addr;
+      log_write(bp);
+    }
+  }
+  brelse(bp);
+  if (clevel == level)
+  {
+    return addr;
+  }
+  else
+  {
+    if (a[toff])
+      return bwalk(ip, &a[toff], off - toff * power(NINDIRECT, level - clevel), level, clevel + 1);
+    else
+      return 0;
+  }
+}
+
 
 // Inode content
 //
@@ -382,13 +429,13 @@ iunlockput(struct inode *ip)
 static uint
 bmap(struct inode *ip, uint bn)
 {
-  uint addr, *a;
-  struct buf *bp;
-
-  if(bn < NDIRECT){
-    if((addr = ip->addrs[bn]) == 0){
+  uint addr;
+  if (bn < NDIRECT)
+  {
+    if ((addr = ip->addrs[bn]) == 0)
+    {
       addr = balloc(ip->dev);
-      if(addr == 0)
+      if (addr == 0)
         return 0;
       ip->addrs[bn] = addr;
     }
@@ -396,62 +443,75 @@ bmap(struct inode *ip, uint bn)
   }
   bn -= NDIRECT;
 
-  if(bn < NINDIRECT){
-    // Load indirect block, allocating if necessary.
-    if((addr = ip->addrs[NDIRECT]) == 0){
-      addr = balloc(ip->dev);
-      if(addr == 0)
-        return 0;
-      ip->addrs[NDIRECT] = addr;
-    }
-    bp = bread(ip->dev, addr);
-    a = (uint*)bp->data;
-    if((addr = a[bn]) == 0){
-      addr = balloc(ip->dev);
-      if(addr){
-        a[bn] = addr;
-        log_write(bp);
-      }
-    }
-    brelse(bp);
-    return addr;
+  // single level indirect block
+  if (bn < NINDIRECT)
+  {
+    return bwalk(ip, &(ip->addrs[NDIRECT]), bn, 1, 1);
+  }
+  bn -= NINDIRECT;
+  // double level indirect block
+  if (bn < NINDIRECT * NINDIRECT)
+  {
+    return bwalk(ip, &(ip->addrs[NDIRECT + 1]), bn, 2, 1);
   }
 
   panic("bmap: out of range");
 }
 
+void bwalkfree(struct inode *ip, uint *cur, uint level, uint clevel)
+{
+  uint j;
+  struct buf *bp = bread(ip->dev, *cur);
+  uint *a = (uint *)bp->data;
+  for (j = 0; j < NINDIRECT; j++)
+  {
+    if (level != clevel)
+    {
+      if (a[j])
+        bwalkfree(ip, &a[j], level, clevel + 1);
+    }
+    else
+    {
+      if (a[j])
+        bfree(ip->dev, a[j]);
+
+    }
+  }
+  brelse(bp);
+  bfree(ip->dev, *cur);
+  *cur = 0;
+}
+
 // Truncate inode (discard contents).
 // Caller must hold ip->lock.
-void
-itrunc(struct inode *ip)
+void itrunc(struct inode *ip)
 {
-  int i, j;
-  struct buf *bp;
-  uint *a;
+  int i;
 
-  for(i = 0; i < NDIRECT; i++){
-    if(ip->addrs[i]){
+  for (i = 0; i < NDIRECT; i++)
+  {
+    if (ip->addrs[i])
+    {
+      if (ip->addrs[i] < 30)
+        panic("itrunc: error\n");
       bfree(ip->dev, ip->addrs[i]);
       ip->addrs[i] = 0;
     }
   }
 
-  if(ip->addrs[NDIRECT]){
-    bp = bread(ip->dev, ip->addrs[NDIRECT]);
-    a = (uint*)bp->data;
-    for(j = 0; j < NINDIRECT; j++){
-      if(a[j])
-        bfree(ip->dev, a[j]);
-    }
-    brelse(bp);
-    bfree(ip->dev, ip->addrs[NDIRECT]);
-    ip->addrs[NDIRECT] = 0;
+  if (ip->addrs[NDIRECT])
+  {
+    bwalkfree(ip, &(ip->addrs[NDIRECT]), 1, 1);
+  }
+
+  if (ip->addrs[NDIRECT + 1])
+  {
+    bwalkfree(ip, &(ip->addrs[NDIRECT + 1]), 2, 1);
   }
 
   ip->size = 0;
   iupdate(ip);
 }
-
 // Copy stat information from inode.
 // Caller must hold ip->lock.
 void
